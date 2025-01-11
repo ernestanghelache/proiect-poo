34b89756e8467ee709bbcec49d6a5ee6f3c10a7e
ernestanghelache <anghelache.ernest@yahoo.com>
2025-01-11 18:43:52 +0200

    Update CarteBucate.cpp

--- sources/CarteBucate.cpp
+++ sources/CarteBucate.cpp
@@ -1,1 +1,22 @@
+#include "CarteBucate.h"
+
+void CarteBucate::adaugaReteta(const Reteta& reteta) {
+    retete.push_back(reteta);
+}
+
+Reteta* CarteBucate::gasesteReteta(const std::string& numeReteta) {
+    for (auto& reteta : retete) {
+        if (reteta.getNumeReteta() == numeReteta) {
+            return &reteta;
+        }
+    }
+    return nullptr;
+}
+
+std::ostream& operator<<(std::ostream& os, const CarteBucate& carte) {
+    for (const auto& reteta : carte.retete) {
+        os << reteta << '\n';
+    }
+    return os;
+}
 
