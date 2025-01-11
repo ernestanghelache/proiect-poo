3225e2b98a301c97e5b2284058e65107e2703a50
ernestanghelache <anghelache.ernest@yahoo.com>
2025-01-11 18:49:37 +0200

    Update Stoc.h

--- sources/Stoc.h
+++ sources/Stoc.h
@@ -1,1 +1,20 @@
+#ifndef STOC_H
+#define STOC_H
 
+#include "Ingredient.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+class Stoc {
+    std::vector<std::unique_ptr<AbstractIngredient>> produse;
+
+public:
+    Stoc();
+    void adaugaIngredient(const AbstractIngredient& ingredient);
+    [[nodiscard]] AbstractIngredient *gasesteProdus(const std::string &numeProdus) const;
+
+    [[nodiscard]] const std::vector<std::unique_ptr<AbstractIngredient>>& getProduse() const;
+};
+
+#endif
