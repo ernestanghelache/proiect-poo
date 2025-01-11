a755109e854e4f6e90dd7cdcd6eebaf24f306dcc
ernestanghelache <anghelache.ernest@yahoo.com>
2025-01-11 18:47:18 +0200

    Update Stoc.cpp

--- sources/Stoc.cpp
+++ sources/Stoc.cpp
@@ -1,1 +1,29 @@
+#include "Stoc.h"
+
+Stoc::Stoc() : produse{} {}
+
+void Stoc::adaugaIngredient(const AbstractIngredient& ingredient) {
+    produse.emplace_back(ingredient.clone());
+}
+
+AbstractIngredient* Stoc::gasesteProdus(const std::string& numeProdus) const {
+    for (auto& produs : produse) {
+        if (produs->getNumeIngredient() == numeProdus) {
+            return produs.get();
+        }
+    }
+    return nullptr;
+}
+
+const std::vector<std::unique_ptr<AbstractIngredient>>& Stoc::getProduse() const {
+    return produse;
+}
+
+std::ostream& operator<<(std::ostream& os, const Stoc& stoc) {
+    os << "Stocul curent:\n";
+    for (const auto& produs : stoc.produse) {
+        produs->afisare();
+    }
+    return os;
+}
 
