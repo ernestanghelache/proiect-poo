2c3b132b6d14bc330ce42b9cc466959835522bf0
ernestanghelache <anghelache.ernest@yahoo.com>
2025-01-11 18:44:56 +0200

    Update Exceptii.h

--- sources/Exceptii.h
+++ sources/Exceptii.h
@@ -1,1 +1,31 @@
+#ifndef EXCEPTII_H
+#define EXCEPTII_H
+
+#include <exception>
+#include <string>
+#include <utility>
+
+class RetetaException : public std::exception {
+    std::string mesaj;
+
+public:
+    explicit RetetaException(std::string  mesaj_) : mesaj(std::move(mesaj_)) {}
+
+    [[nodiscard]] const char* what() const noexcept override {
+        return mesaj.c_str();
+    }
+};
+
+class IngredientException final : public std::exception {
+    std::string mesaj;
+
+public:
+    explicit IngredientException(std::string  mesaj_) : mesaj(std::move(mesaj_)) {}
+
+    [[nodiscard]] const char* what() const noexcept override {
+        return mesaj.c_str();
+    }
+};
+
+#endif
 
