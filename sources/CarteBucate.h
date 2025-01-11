6e10a38287a2dd3b1f41721464125807cda64d69
ernestanghelache <anghelache.ernest@yahoo.com>
2025-01-11 18:44:36 +0200

    Update CarteBucate.h

--- sources/CarteBucate.h
+++ sources/CarteBucate.h
@@ -1,1 +1,18 @@
+#ifndef CARTEBUCATE_H
+#define CARTEBUCATE_H
+
+#include "Reteta.h"
+#include <vector>
+
+class CarteBucate {
+    std::vector<Reteta> retete;
+
+public:
+    void adaugaReteta(const Reteta& reteta);
+    Reteta* gasesteReteta(const std::string& numeReteta);
+
+    friend std::ostream& operator<<(std::ostream& os, const CarteBucate& carte);
+};
+
+#endif
 
